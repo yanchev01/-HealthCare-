89347c7cfed282e178aebec26e1eb7e891c23e4f
Владимир Соколов <sokolover.heh@gmail.com>
2024-04-27 23:10:34 +0300

    first1

--- app/src/main/java/com/example/myhealthapplication/LoginActivity.java
+++ app/src/main/java/com/example/myhealthapplication/LoginActivity.java
@@ -2,13 +2,51 @@ package com.example.myhealthapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    EditText eUsername, edPassword;
+    Button btn;
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        eUsername = findViewById(R.id.editTextUsername);
+        edPassword = findViewById(R.id.editTextPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewRegister);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = eUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if(username.length()==0 || password.length()==0){
+                    Toast.makeText(getApplicationContext(), "Заполните все пункты", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Вход выполнен", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myhealthapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myhealthapplication/RegisterActivity.java
@@ -2,13 +2,94 @@ package com.example.myhealthapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText edUsername, edPassword, edConfirm, edEmail;
+    Button btn;
+    TextView tv;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edUsername = findViewById(R.id.editTextRegUsername);
+        edPassword = findViewById(R.id.editTextRegPassword);
+        edEmail = findViewById(R.id.editTextEmail);
+        edConfirm = findViewById(R.id.editTextPasswordConfirm);
+        btn = findViewById(R.id.buttonRegister);
+        tv = findViewById(R.id.textViewRegistration);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUsername.getText().toString();
+                String email = edEmail.getText().toString();
+                String password = edPassword.getText().toString();
+                String confirm = edConfirm.getText().toString();
+                Database db = new Database(getApplicationContext(),"healthcare",null,1);
+                if(username.length()==0 || email.length()==0 || password.length()==0 || confirm.length()==0) {
+                    Toast.makeText(getApplicationContext(), "Заполните все пункты", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if(password.compareTo(confirm)==0){
+                        if(isValid(password)) {
+                            db.register(username, email, password);
+                            Toast.makeText(getApplicationContext(), "Регистрация пройдена", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Пароль слишком длинный и т.п", Toast.LENGTH_SHORT).show();
+                            }
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Пароли не совпадают и т.п", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+    public static boolean isValid(String passwordhere) {
+        int f1 = 0, f2 = 0, f3 = 0;
+        if (passwordhere.length() < 8) {
+            return false;
+        } else {
+            for (int p = 0; p < passwordhere.length(); p++) {
+                if (Character.isLetter(passwordhere.charAt(p))) {
+                    f1 = 1;
+                }
+            }
+            for (int r = 0; r < passwordhere.length(); r++) {
+                if (Character.isDigit(passwordhere.charAt(r))) {
+                    f2 = 1;
+                }
+            }
+            for (int s = 0; s < passwordhere.length(); s++) {
+                char c = passwordhere.charAt(s);
+                if (c >= 33 && c <= 46 || c == 64) {
+                    f3 = 1;
+                }
+            }
+            if (f1 == 1 && f2 == 1 && f3 == 1)
+                return true;
+            return false;
+        }
     }
 }
