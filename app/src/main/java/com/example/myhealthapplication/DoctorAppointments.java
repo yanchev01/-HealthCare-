91b3520a4341dc635b4d86c1119eaff7a3e2b22c
Владимир Соколов <sokolover.heh@gmail.com>
2024-05-06 18:04:27 +0300

    doctors

---
+++ app/src/main/java/com/example/myhealthapplication/DoctorAppointments.java
@@ -0,0 +1,14 @@
+package com.example.myhealthapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorAppointments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_appointments);
+    }
+}
---
+++ app/src/main/java/com/example/myhealthapplication/GeneralPractitioner.java
@@ -0,0 +1,14 @@
+package com.example.myhealthapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneralPractitioner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_practitioner);
+    }
+}
