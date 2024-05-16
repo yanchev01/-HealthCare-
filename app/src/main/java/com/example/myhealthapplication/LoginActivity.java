496d93f42a44477137501ae5c7984bd7689ae9ca
Andrey Romanov <yanchev1@vk.com>
2024-05-16 04:58:45 +0300

    fixes

--- app/src/main/java/com/example/myhealthapplication/LoginActivity.java
+++ app/src/main/java/com/example/myhealthapplication/LoginActivity.java
@@ -1,70 +1,17 @@
 package com.example.myhealthapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
-
-    EditText eUsername, edPassword;
-    Button btn;
-    TextView tv;
-    Database db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        eUsername = findViewById(R.id.editTextUsername);
-        edPassword = findViewById(R.id.editTextPassword);
-        btn = findViewById(R.id.buttonLogin);
-        tv = findViewById(R.id.textViewRegister);
-
-        db = new Database();
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = eUsername.getText().toString();
-                String password = edPassword.getText().toString();
-
-                if (username.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Заполните все пункты", Toast.LENGTH_SHORT).show();
-                } else {
-                    db.login(username, password, new Database.LoginCallback() {
-                        @Override
-                        public void onSuccess() {
-                            Toast.makeText(getApplicationContext(), "Вход выполнен", Toast.LENGTH_SHORT).show();
-                            SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            editor.putString("username", username);
-                            editor.apply();
-                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-                        }
-
-                        @Override
-                        public void onFailure(String errorMessage) {
-                            Toast.makeText(getApplicationContext(), "Ошибка: " + errorMessage, Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-            }
-        });
-
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-            }
-        });
     }
 }
